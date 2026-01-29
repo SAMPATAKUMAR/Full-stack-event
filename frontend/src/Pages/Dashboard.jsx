b9d19f7eab84f570138c245df3b56da59954288f
SAMPATAKUMAR V <148532254+SAMPATAKUMAR@users.noreply.github.com>
2026-01-30 00:08:55 +0530

    theme

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../style/theme.css";
+import "../Style/theme.css";
 import "../Style/dashboard.css";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
